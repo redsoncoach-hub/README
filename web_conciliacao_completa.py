4eaa3e5974411d5c1926dd8754dbcc232d2a337d
redsoncoach-hub <redsoncoach@gmail.com>
2025-10-10 23:38:18 -0300

    Create web_conciliacao_completa.py

---
+++ web_conciliacao_completa.py
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Protótipo completo conciliação"
+git branch -M main
+git remote add origin https://github.com/SEU_USUARIO/conciliacao-cisslive.git
+git push -u origin main
